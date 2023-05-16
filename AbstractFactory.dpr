71a32e88afc172440b752dc921537f044849f3c9
iMont-F <gabriel.fz250@outlook.com>
2023-05-16 20:36:06 -0300

    [IM-0001] | (*) AbstractFactory - Corrigido erro ortográfico;                            (*) AbstractFactory - Ajustado nome do formulário;

--- AbstractFactory.dpr
+++ AbstractFactory.dpr
@@ -2,13 +2,17 @@ program AbstractFactory;
 
 uses
   Vcl.Forms,
-  usMain in 'usMain.pas' {Form1};
+  usMain in 'usMain.pas' {frmConcentrador},
+  Pattern.AbstractProduct in 'Pattern.AbstractProduct.pas',
+  Pattern.AbstractFactory in 'Pattern.AbstractFactory.pas',
+  Pattern.ConcreteFactory in 'Pattern.ConcreteFactory.pas',
+  Pattern.Product.Concentrador in 'Pattern.Product.Concentrador.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmConcentrador, frmConcentrador);
   Application.Run;
 end.
